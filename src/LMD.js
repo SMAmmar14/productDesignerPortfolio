e4f7fffc61e1481adb5eec68668fa53e0f710520
Syed Mohammad Ammar <34889648+SMAmmar14@users.noreply.github.com>
2022-04-06 23:01:44 +0500

    added user persona

--- src/LMD.js
+++ src/LMD.js
@@ -10,7 +10,7 @@ import DeliveryResults from '../src/Components/Assets/Img/DeliveryResults.gif';
 import DeliveriesPerHour from '../src/Components/Assets/Img/DeliveriesPerHour.gif';
 import LMDHeader from '../src/Components/Assets/Img/coverphoto.png';
 import LMDMultiModal from '../src/Components/Assets/Img/MultiModal.png';
-import LMDPersona from '../src/Components/Assets/Img/persona.jpeg';
+import LMDPersona from '../src/Components/Assets/Img/userpersona.jpeg';
 import SubwayUncle from '../src/Components/Assets/Img/PrimaryResearch.png';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import { LinkContainer } from 'react-router-bootstrap';
