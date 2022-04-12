341cb95a2065db25b504c76366daf7112a12147c
Syed Mohammad Ammar <34889648+SMAmmar14@users.noreply.github.com>
2022-04-13 03:54:32 +0500

    added drrible link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,12 @@
- import React from 'react';
- import styled from 'styled-components';
- import {Figma} from 'react-feather';
- import {Mail} from 'react-feather';
- import {Twitter} from 'react-feather';
- import {NavLink} from 'react-router-dom';
-  
- 
- const Styles = styled.div`
+import React from 'react';
+import styled from 'styled-components';
+import { Figma } from 'react-feather';
+import { Mail } from 'react-feather';
+import { Twitter } from 'react-feather';
+import { NavLink } from 'react-router-dom';
+
+
+const Styles = styled.div`
 
 
  @media only screen and (max-width: 768px) { 
@@ -94,49 +94,51 @@
 
  `;
 
- export const Footer = () => (
+export const Footer = () => (
     <Styles>
-    <footer className="footerStyle">
-    <div className="container">
-    <div className="row footerPadding">
-        <div className="col-md-4">
-        <h1 className = "footerHeader">Syed Mohammad Ammar</h1>
-        <p className="footerSubHeader text-justify"> Product designer at CAKE based in California.</p>
+        <footer className="footerStyle">
+            <div className="container">
+                <div className="row footerPadding">
+                    <div className="col-md-4">
+                        <h1 className="footerHeader">Syed Mohammad Ammar</h1>
+                        <p className="footerSubHeader text-justify"> Product designer at CAKE based in California.</p>
 
-        <ul className = "footerList">
-        <li className = "listStyle mb-2"><Mail strokeWidth="1.5" color = "#333333" size = {20}></Mail> <a href="mailto:furquan101@gmail.com">Email me</a></li>
-        <li className = "mb-4"> <Twitter strokeWidth="1.5" color = "#333333" size = {20}></Twitter> <a href="https://twitter.com/Ammar110_SM" target="_blank" rel="noopener noreferrer">@SMAmmar5</a></li>
-        </ul>
-        </div>
-        <div className="offset-md-1 col-xs-6 col-md-2">
-        <h2 className="footerBrowsingHeader">Keep Browsing</h2>
-        <ul className="footerList">
-            <li className = "mb-1">
-                <NavLink to = "/">Portfolio</NavLink>
-            </li>
-            <li className = "mb-1"><a rel="noopener noreferrer" target="_blank" href="https://drive.google.com/file/d/1NyDxyLLQ2BkDDH9UGDRgPHI76dq78KNa/view?usp=sharing">Resume</a></li>
-            <li className = "mb-1"><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/syed-mohammad-ammar-053877145/">LinkedIn</a></li>
-   
+                        <ul className="footerList">
+                            <li className="listStyle mb-2"><Mail strokeWidth="1.5" color="#333333" size={20}></Mail> <a href="mailto:furquan101@gmail.com">Email me</a></li>
+                            <li className="mb-4"> <Twitter strokeWidth="1.5" color="#333333" size={20}></Twitter> <a href="https://twitter.com/Ammar110_SM" target="_blank" rel="noopener noreferrer">@SMAmmar5</a></li>
+                        </ul>
+                    </div>
+                    <div className="offset-md-1 col-xs-6 col-md-2">
+                        <h2 className="footerBrowsingHeader">Keep Browsing</h2>
+                        <ul className="footerList">
+                            <li className="mb-1">
+                                <NavLink to="/">Portfolio</NavLink>
+                            </li>
+                            <li className="mb-1"><a rel="noopener noreferrer" target="_blank" href="https://drive.google.com/file/d/1NyDxyLLQ2BkDDH9UGDRgPHI76dq78KNa/view?usp=sharing">Resume</a></li>
+                            <li className="mb-1"><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/syed-mohammad-ammar-053877145/">LinkedIn</a></li>
+                            <li className="mb-1"><a rel="noopener noreferrer" target="_blank" href="https://dribbble.com/SMAmmar14">Dribble</a></li>
 
-            <li className = "mb-1">
-                <NavLink to = "/About">About</NavLink>
-            </li>
-         
-        </ul>
-        </div>
 
-    </div>
-    <hr/>
-    </div>
-    <div className="container">
-    <div className="row">
-        <div className="col-12">
-        <p className="copyrightText">Copyright © {new Date().getFullYear()} Syed Mohammad Ammar.&nbsp; 
-            <a rel="noopener noreferrer" target="_blank" href="https://github.com/SMAmmar14">Developed by SMAmmar .</a>
-        </p>
-        </div>
-    </div>
-    </div>
-    </footer>
+
+                            <li className="mb-1">
+                                <NavLink to="/About">About</NavLink>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+                <hr />
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <p className="copyrightText">Copyright © {new Date().getFullYear()} Syed Mohammad Ammar.&nbsp;
+                            <a rel="noopener noreferrer" target="_blank" href="https://github.com/SMAmmar14">Developed by SMAmmar .</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </Styles>
- )
+)
